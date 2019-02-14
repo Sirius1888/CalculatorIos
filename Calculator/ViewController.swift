2cfe3a88b32754b5b1af47b2694aeed742ce4a65
sirius <sirius@MacBook-Pro-sirius.local>
2019-02-14 09:34:11 +0600

    added storyBoard

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by sirius on 14.02.2019.
+//  Copyright © 2019 sirius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
